5c6868a20c1ae56aa8e43f4ece0ae723d1d86d81
Angel Rodriguez <angel.rodriguez@unizar.es>
2026-06-12 10:45:24 +0200

    Code update (pressures modified) 	modified:   doc/archiver/postgres_addPV.sql 	modified:   doc/modbus/modbus_chiller.py 	modified:   dqApp/Db/chiller.db 	modified:   opi/dqchiller.bob 	new file:   opi/presTemp.plt

--- doc/archiver/postgres_addPV.sql
+++ doc/archiver/postgres_addPV.sql
@@ -1,14 +1,21 @@
 --INSERT INTO channel(channel_id, name) VALUES (1, 'uno:DHT_HT');
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:Tout',1,1,9999);
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:ChSetPointRB',1,1,9999);
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:HPSetPointRB',1,1,9999);
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:PexpV',1,1,9999);
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:Pout',1,1,9999);
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:Tevap',1,1,9999);
-INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:TexpV',1,1,9999);
+--INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:Tout',1,1,9999);
+--INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:ChSetPointRB',1,1,9999);
+--INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:PexpV',1,1,9999);
+--INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:Pcir1',1,1,9999);
+--INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:Tevap',1,1,9999);
+--INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:TexpV',1,1,9999);
+
+INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:RelayStatus1_Alarm',1,1,9999);
+INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:RelayStatus1_WaterPump',1,1,9999);
+INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:RelayStatus2_CondensingFan',1,1,9999);
+INSERT INTO channel(name,smpl_mode_id,grp_id,retent_val) VALUES ( 'DQ:CHL:RelayStatus7_Compressor',1,1,9999);
 
 
 --UPDATE channel SET smpl_per=1 WHERE channel_id < 100;
 --UPDATE channel SET smpl_mode_id=2 WHERE channel_id < 100;
 --UPDATE channel SET grp_id=1 WHERE channel_id < 100;
 --UPDATE channel SET retent_val=9999 WHERE channel_id < 100;
+--UPDATE channel SET name=Pcir1 WHERE channel_id < 8;
+--DELETE FROM channel where channel_id = 9;
+--DELETE FROM sample where channel_id = 9;
--- doc/modbus/modbus_chiller.py
+++ doc/modbus/modbus_chiller.py
@@ -17,7 +17,7 @@ client.connect()
 #print(registers)
 
 
-result = client.read_holding_registers(address=3590, count=1 )# , slave=1)
+result = client.read_holding_registers(address=2304, count=6 )# , slave=1)
 #result = client.read_holding_registers(address=3584, count=1 )# , slave=1)
 #print(result.isError())
 #print(result.registers)
