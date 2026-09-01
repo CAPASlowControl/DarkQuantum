c4a504d24ac55b097c7e69b086fdeb9d18491185
Angel Rodriguez <angel.rodriguez@unizar.es>
2026-09-01 12:19:04 +0200

    BFIOC: Turbo1 TC400 added 	new file:   dqApp/Db/bf_tc400.db 	modified:   iocBoot/iocdq/st_bf.cmd

--- iocBoot/iocdq/st_bf.cmd
+++ iocBoot/iocdq/st_bf.cmd
@@ -26,6 +26,7 @@ drvAsynIPPortConfigure("bf_cpa","127.0.0.1:49099",0,0,1)
 drvAsynIPPortConfigure("bf_tcch","127.0.0.1:49099",0,0,1)
 drvAsynIPPortConfigure("bf_tcht","127.0.0.1:49099",0,0,1)
 drvAsynIPPortConfigure("bf_nxds","127.0.0.1:49099",0,0,1)
+drvAsynIPPortConfigure("bf_tc400","127.0.0.1:49099",0,0,1)
 
 #Load Records
 dbLoadRecords("$(TOP)/dqApp/Db/bf_mpbf.db","P=DQ:BF:MPBF,PORT=bf_mpbf") # Mapper Bf with main variables.
@@ -33,6 +34,7 @@ dbLoadRecords("$(TOP)/dqApp/Db/bf_cpa.db","P=DQ:BF:CPA,PORT=bf_cpa") # Compresso
 dbLoadRecords("$(TOP)/dqApp/Db/bf_tcch.db","P=DQ:BF:TCCH,PORT=bf_tcch") # Temperature controller channels
 dbLoadRecords("$(TOP)/dqApp/Db/bf_tcht.db","P=DQ:BF:TCHT,PORT=bf_tcht") # Temperature controller Heaters
 dbLoadRecords("$(TOP)/dqApp/Db/bf_nxds.db","P=DQ:BF:NXDS,PORT=bf_nxds") # xnds: Edwars Scroll Pump (Scroll1)
+dbLoadRecords("$(TOP)/dqApp/Db/bf_tc400.db","P=DQ:BF:TC400,PORT=bf_nxds") # Peiffer HiPACE TC400 (TURBO 1)
 
 
 #cd "${TOP}/iocBoot/${IOC}"
